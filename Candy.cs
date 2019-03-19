884f9781181d2a80e3515830340f4b965058ec82
Colin White <clwhite814@gmail.com>
2019-03-19 18:34:49 -0500

    narrowed flavors down to 10

--- Candy.cs
+++ Candy.cs
@@ -6,7 +6,6 @@ namespace candy_market
     {
         public string Name { get; set; }
         public FlavorCategory FlavorCategory { get; set; }
-
         public DateTime DateReceived { get; set; }
         public bool isEaten { get; set; }
         public string Manufacturer { get; set; }
