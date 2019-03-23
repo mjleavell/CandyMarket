79af2649120533e8038a93964576c3d7bb6257e5
MCrank <marco_mcse@hotmail.com>
2019-03-23 10:26:31 -0500

    Refactor menus and validate the Quantity is an int.

--- Menus/AddCandyMenu.cs
+++ Menus/AddCandyMenu.cs
@@ -8,37 +8,25 @@ namespace candy_market.Menus
     {
         internal static void AddCandyMenus(CandyStorage db, int userId)
         {
-            //var flavorCategoryString = string.Join(",", flavorCategory);
-            View addCandyMenuName = new View()
-                    .AddMenuOption("Please provide the candy name:")
-                    .AddMenuText("Press Esc to exit.");
-            Console.Write(addCandyMenuName.GetFullMenu());
-            var newCandyName = Console.ReadLine();
+            int newCandyQuantity = 0;
 
-            View addCandyMenuFlavor = new View()
-                    .AddMenuOption("Please provide a flavor or type:")
-                    .AddMenuText("Press Esc to exit.");
-            Console.Write(addCandyMenuFlavor.GetFullMenu());
-            var newCandyFlavor = Console.ReadLine();
+            var newCandyName = AddCandyMenuName();
 
-            View addCandyMenuManufacturer = new View()
-                   .AddMenuOption("Please provide the manufacturer:")
-                   .AddMenuText("Press Esc to exit.");
-            Console.Write(addCandyMenuManufacturer.GetFullMenu());
-            var newCandyManufacturer = Console.ReadLine();
+            var newCandyFlavor = AddCandyMenuFlavor();
 
-            View addCandyMenuQuantity = new View()
-                    .AddMenuOption("Please provide the Quantity you want to add:")
-                    .AddMenuText("Press Esc to exit.");
-            Console.Write(addCandyMenuQuantity.GetFullMenu());
-            var newCandyQuantity = Console.ReadLine();
+            var newCandyManufacturer = AddCandyMenuManufacturer();
+
+            do
+            {
+                newCandyQuantity = AddCandyMenuQuantity();
+            } while (newCandyQuantity < 1);
 
             AddNewCandy(db, newCandyName, newCandyFlavor, newCandyManufacturer, newCandyQuantity, userId);
         }
 
-        internal static void AddNewCandy(CandyStorage db, string newCandyName, string newCandyFlavor, string newCandyManufacturer, string newCandyQuantity, int userId)
+        internal static void AddNewCandy(CandyStorage db, string newCandyName, string newCandyFlavor, string newCandyManufacturer, int newCandyQuantity, int userId)
         {
-            for (int i = 0; i < int.Parse(newCandyQuantity); i++)
+            for (int i = 0; i < newCandyQuantity; i++)
             {
                 var newCandy = new Candy(newCandyName, newCandyFlavor, DateTime.Now, newCandyManufacturer, userId);
                 var savedCandy = db.SaveNewCandy(newCandy);
@@ -48,6 +36,56 @@ namespace candy_market.Menus
             Console.WriteLine("Press the 'Enter' key to continue.");
             Console.ReadLine();
         }
-       
+
+        private static string AddCandyMenuName()
+        {
+            View addCandyMenuName = new View()
+                    .AddMenuOption("Please provide the candy name:")
+                    .AddMenuText("Press Esc to exit.");
+            Console.Write(addCandyMenuName.GetFullMenu());
+            var newCandyName = Console.ReadLine();
+            return newCandyName;
+        }
+
+        private static string AddCandyMenuFlavor()
+        {
+            View addCandyMenuFlavor = new View()
+                    .AddMenuOption("Please provide a flavor or type:")
+                    .AddMenuText("Press Esc to exit.");
+            Console.Write(addCandyMenuFlavor.GetFullMenu());
+            var newCandyFlavor = Console.ReadLine();
+            return newCandyFlavor;
+        }
+
+        private static string AddCandyMenuManufacturer()
+        {
+            View addCandyMenuManufacturer = new View()
+                   .AddMenuOption("Please provide the manufacturer:")
+                   .AddMenuText("Press Esc to exit.");
+            Console.Write(addCandyMenuManufacturer.GetFullMenu());
+            var newCandyManufacturer = Console.ReadLine();
+            return newCandyManufacturer;
+        }
+
+        private static int AddCandyMenuQuantity()
+        {
+            View addCandyMenuQuantity = new View()
+                    .AddMenuOption("Please provide the Quantity you want to add:")
+                    .AddMenuText("Press Esc to exit.");
+            Console.Write(addCandyMenuQuantity.GetFullMenu());
+            var newCandyQuantity = Console.ReadLine();
+            try
+            {
+                return int.Parse(newCandyQuantity);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"The quantity you entered is not valid please try again");
+                Console.WriteLine("Press the 'Enter' key to try again");
+                Console.ReadLine();
+                return 0;
+
+            }
+        }
     }
 }
