23241705620d52a559b7915feeec52c1a67d4121
Tim Harley <Timothy.D.Harley@gmail.com>
2019-03-25 16:28:25 -0500

    fixed case sensitivity and added error messages for null candy

--- Menus/TradeCandyMenu.cs
+++ Menus/TradeCandyMenu.cs
@@ -9,6 +9,7 @@ namespace candy_market.Menus
     {
         public static void AddTradeCandyMenu(CandyStorage db, int userId, Users user, List<Users> candyUsers)
         {
+            //Screen for choosing who to trade with
             View candyMenuTradeWho = new View()
                     .AddMenuText($"Hello {user.Name}. Who would you like to trade with?")
                     .AddMenuOptions(candyUsers.Select(u => u.Name).ToList());
@@ -18,6 +19,13 @@ namespace candy_market.Menus
             var ParsedTradeUser = int.Parse(userInput);
             var CandyTradeWho = DisplayUserMenu.GetUser(ParsedTradeUser, candyUsers);
 
+            if(CandyTradeWho.Id == userId)
+            {
+                Console.WriteLine("\nYou want to trade with yourself?  That seems like a dumb waste of your time, but knock yourself out. . .");
+                Console.ReadLine();
+            }
+
+            //Screen for choosing what candy you want
             var theirCandyBag = db.GetCandyByUserId(CandyTradeWho.Id);
             var listTheirCandyBag = theirCandyBag.Select(x => x.Name).ToList();
             var showTheirCandyBag = string.Join("\n", listTheirCandyBag);
@@ -28,6 +36,7 @@ namespace candy_market.Menus
             Console.Write(candyMenuRecieveWhat.GetFullMenu());
             var CandyRecieveWhat = Console.ReadLine();
 
+            //Screen for choosing which candy to give in return
             var myCandyBag = db.GetCandyByUserId(userId);
             var listMyCandyBag = myCandyBag.Select(x => x.Name).ToList();
             var showMyCandyBag = string.Join("\n", listMyCandyBag);
@@ -41,6 +50,7 @@ namespace candy_market.Menus
             TradeCandy(db, userId, CandyTradeWho, CandyRecieveWhat, CandyGiveWhat);
         }
 
+        //Trade Logic
         internal static void TradeCandy(CandyStorage db, int userId, Users CandyTradeWho, string CandyRecieveWhat, string CandyGiveWhat)
         {
             var MyCandyBag = db.GetCandyByUserId(userId);
@@ -49,11 +59,23 @@ namespace candy_market.Menus
             var TheirCandyBag = db.GetCandyByUserId(CandyTradeWho.Id);
             var PieceToGet = TheirCandyBag.Find(x => x.Name == CandyRecieveWhat);
 
-            PieceToGet.UserId = userId;
-            PieceToTrade.UserId = CandyTradeWho.Id;
-
-            Console.WriteLine($"You traded your {PieceToTrade.Name} for {CandyTradeWho.Name}'s {PieceToGet.Name}!");
-            Console.ReadLine();
+            if(PieceToGet != null && PieceToTrade != null)
+            {
+                PieceToGet.UserId = userId;
+                PieceToTrade.UserId = CandyTradeWho.Id;
+                Console.WriteLine($"You traded your {PieceToTrade.Name} for {CandyTradeWho.Name}'s {PieceToGet.Name}!");
+                Console.ReadLine();
+            }
+            else if(PieceToGet == null)
+            {
+                Console.WriteLine($"Error.  {CandyTradeWho.Name} does not have any {CandyRecieveWhat} to trade.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"Error. You do not have any {CandyGiveWhat} to trade.");
+                Console.ReadLine();
+            }
         }
     }
 }
